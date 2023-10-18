e2e08404c9212edd0a2ef173ac544e33b5c0828c
ubaid4j <ubaidshan007@gmail.com>
2023-10-18 09:17:42 +0500

    init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "Ubaid", Price: 44.1},
+	{ID: "2", Title: "Yellow Train", Artist: "Attiq", Price: 49.1},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.Run("localhost:8080")
+}
